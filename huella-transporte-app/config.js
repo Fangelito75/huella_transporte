bd3c1e136f668adf190647f38da59de54e8a3ab9
Fangelito75 <112319125+Fangelito75@users.noreply.github.com>
2026-07-21 09:50:59 +0200

    Update config.js

--- huella-transporte-app/config.js
+++ huella-transporte-app/config.js
@@ -4,7 +4,7 @@
   // Después de desplegar google-apps-script/Code.gs como aplicación web,
   // pega aquí la URL terminada en /exec. No añadas claves ni contraseñas.
   window.RUTACO2_CONFIG = Object.freeze({
-    apiUrl: "",
+    apiUrl: "script.google.com/macros/s/AKfycbxBPEF3l7gtZ83GRK1OjTk-eMTtcXDMcUkobK4NU-S4zKuz3gFmR8xmQpjON_P83r44ng/exec",
     minimumSampleSize: 5,
   });
 }());
